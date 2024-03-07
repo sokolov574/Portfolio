37692a75dc23c1311c4b088159720e5ded036fbc
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-07 16:01:56 +0100

    added logo for project 1

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,29 +1,20 @@
 export const projects = [
   {
     title: 'eAmt',
-    description: "soon",
-      image: '/#',
+    description: "E-AMT is an online platform designed to facilitate official procedures in Germany. It offers a convenient way for users to complete various official tasks over the internet, making government interactions more accessible and efficient.",
+      image: '../images/logo_eAmt.png',
       tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://e-amt.vercel.app/en/dashboard',
+    visit: 'https://github.com/sokolov574/eAmt',
     id: 0,
-  },
-  {
-    title: 'Peaky Blinders',
-    description:"soon",
-    image: '/#',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
   },
   {
     title: 'Marvel Portal',
     description: "soon",
-      image: '/#',
-      tags: ['React'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+      image: '../images/logo.png',
+      tags: ['React', 'JavaScript'],
+    source: 'https://sokolov574.github.io/marvel-portal-react/',
+    visit: 'https://github.com/sokolov574/marvel-portal-react',
     id: 2,
   },
   {
@@ -35,6 +26,17 @@ export const projects = [
     visit: 'https://google.com',
     id: 3,
   },
+  {
+    title: 'Peaky Blinders',
+    description:"soon",
+    image: '/#',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+ 
+  
 ];
 
 export const TimeLineData = [
